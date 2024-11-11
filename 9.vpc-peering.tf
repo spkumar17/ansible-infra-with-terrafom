32311cd19f8b03257d99a3c1a9743e6ab6df94eb
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-11-11 15:41:29 +0530

    changed values

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -4,7 +4,7 @@ data "aws_vpc" "ansible_vpc" {
 
 data "aws_route_table" "ansible_vpc_rt" {
   
-  subnet_id = "subnet-04be945624d68825e"
+  route_table_id = "rtb-05880d5dbfa89bf8e"
   #If subnet_id giving errors use route table id as below
   #route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
@@ -28,7 +28,7 @@ resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
 
 resource "aws_route" "peering-to-ansible-vpc" {
   route_table_id            = aws_route_table.terraform-public.id
-  destination_cidr_block    = "10.0.0.0/16"
+  destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
   #depends_on                = [aws_route_table.terraform-public]
 }
